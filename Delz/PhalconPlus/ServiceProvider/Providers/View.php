573c8d2e3e1d4d0e4cda9f4aa68fdd405f925100
徐威 <sheawey@gmail.com>
2018-07-06 16:05:23 +0800

    修正控制器下子目录默认模板问题

--- Delz/PhalconPlus/ServiceProvider/Providers/View.php
+++ Delz/PhalconPlus/ServiceProvider/Providers/View.php
@@ -7,6 +7,8 @@ namespace Delz\PhalconPlus\ServiceProvider\Providers;
 use Delz\PhalconPlus\ServiceProvider\Provider;
 use Phalcon\Mvc\View as PhalconView;
 use Delz\PhalconPlus\App\IApp;
+use Delz\PhalconPlus\Event\Manager as EventManager;
+use Phalcon\Mvc\Router;
 
 /**
  * 视图服务
@@ -41,6 +43,25 @@ class View extends Provider
                         '.php' => 'phpEngine'
                     ]
                 );
+                /** @var EventManager $eventManager */
+                $eventManager = $self->di->get('eventsManager');
+                $eventManager->attach('application:beforeHandleRequest', function () use ($self, $view) {
+                    /** @var Router $router */
+                    $router = $self->di->get('router');
+                    if ($route = $router->getMatchedRoute()) {
+                        $paths = $route->getPaths();
+                        $namespace = $paths['namespace'];
+                        $controller = ucfirst($paths['controller']);
+                        $action = $paths['action'];
+                        $defaults = $router->getDefaults();
+                        $defaultNamespace = $defaults['namespace'];
+                        if ($namespace != $defaultNamespace && strpos($namespace, $defaultNamespace) === 0) {
+                            $len = strlen($namespace) - strlen($defaultNamespace) - 1;
+                            $viewDir = substr($namespace, -$len) . '/' . $controller . '/' . $action;
+                            $view->pick($viewDir);
+                        }
+                    }
+                });
                 return $view;
             }
         );

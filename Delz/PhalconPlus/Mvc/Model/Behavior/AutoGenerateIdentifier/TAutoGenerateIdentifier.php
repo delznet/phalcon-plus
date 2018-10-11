ea217e6f6e59ba06fe50b62abaa7311907a4ca7b
徐威 <sheawey@gmail.com>
2018-10-11 09:54:31 +0800

    增加AutoGenerateIdentifier Behavior

---
+++ Delz/PhalconPlus/Mvc/Model/Behavior/AutoGenerateIdentifier/TAutoGenerateIdentifier.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Delz\PhalconPlus\Mvc\Model\Behavior\AutoGenerateIdentifier;
+
+/**
+ * 自增Id trait
+ *
+ * @package Delz\PhalconPlus\Mvc\Model\Behavior\AutoGenerateIdentifier
+ */
+trait TAutoGenerateIdentifier
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * 返回自增Id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}

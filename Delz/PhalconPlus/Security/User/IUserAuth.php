cb909c973ad8df98f9b81f25a4e7dbfee827101e
徐威 <sheawey@gmail.com>
2018-11-08 19:39:51 +0800

    [add]增加IUserAuth接口

---
+++ Delz/PhalconPlus/Security/User/IUserAuth.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Delz\PhalconPlus\Security\User;
+
+/**
+ * 可以通过第三方oauth，username，email，mobile查找用户
+ *
+ * IUserAuth保存这些唯一标记
+ *
+ * @package Delz\PhalconPlus\Security\User
+ */
+interface IUserAuth
+{
+    /**
+     * @return IUser
+     */
+    public function getUser(): ?IUser;
+
+    /**
+     * 获取唯一id
+     *
+     * @return string
+     */
+    public function getIdentifier(): string;
+
+    /**
+     * 唯一值类型
+     *
+     * @return string
+     */
+    public function getIdentifierType(): string;
+}
